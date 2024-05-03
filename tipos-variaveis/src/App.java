5b3faf3df54b8e73eead8d65270e6e6f9ce49f73
michaelfranca123 <franca.michaelluiz@gmail.com>
2024-05-03 19:59:02 -0300

    criando o projeto para exemplo de tipos e variaveis

---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto =1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+    }
+}
